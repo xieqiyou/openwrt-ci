6f609fbadb966f5ba86944a7233e027208119513
xieqiyou <xieqiming1987@gmail.com>
2025-11-30 16:26:46 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -2,11 +2,9 @@ rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-./scripts/feeds update -a
 git clone --depth 1 https://github.com/sbwml/openwrt_pkgs.git package/new/custom
 mv package/new/custom/luci-app-netspeedtest  package/new
 mv package/new/custom/speedtest-cli package/new
 rm -rf package/new/custom
-rm -rf feeds.conf.default
-cp ../feeds.conf.default feeds.conf.default
-./scripts/feeds install -a
+
+
