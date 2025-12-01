66474e67bac3ea7ea11c7aab952b26ce72bea355
xieqiyou <xieqiming1987@gmail.com>
2025-12-02 00:16:00 +0800

    Update feed.sh

--- feed.sh
+++ feed.sh
@@ -2,6 +2,8 @@ rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 cp ../0002-upgrade-cmake-min-version.patch package/libs/libwebsockets feeds/packages/libs/libwebsockets/patches/
+cp ../003-chore-bump-minimum-CMake-to-2.8.12.patch feeds/packages/utils/tini/patches/
+cp ../004-chore-allow-CMake-though-to-3.10.patch feeds/packages/utils/tini/patches/
 git clone --depth 1 https://github.com/sbwml/openwrt_pkgs.git package/new/custom
 mv package/new/custom/luci-app-netspeedtest  package/new
 mv package/new/custom/speedtest-cli package/new
