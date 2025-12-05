ee3476cbaf45f82e06eb523a160d27df1a7b98da
xieqiyou <xieqiming1987@gmail.com>
2025-12-05 10:36:56 +0800

    Update feed.sh

--- feed.sh
+++ feed.sh
@@ -5,7 +5,7 @@ chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app
 #cp ../003-chore-bump-minimum-CMake-to-2.8.12.patch feeds/packages/utils/tini/patches/
 #cp ../004-chore-allow-CMake-though-to-3.10.patch feeds/packages/utils/tini/patches/
 git clone --depth 1 https://github.com/sbwml/openwrt_pkgs.git package/new/custom
-rm -rf /feeds/packages/net/speedtest-cli
+rm -rf feeds/packages/net/speedtest-cli
 mv package/new/custom/luci-app-netspeedtest  package/new
 mv package/new/custom/speedtest-cli package/new
 rm -rf package/new/custom
